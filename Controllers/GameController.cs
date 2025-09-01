d259f9fc02e1b1d9ed7b268a0013d56ab56cc2f6
vZidv <diz.shulinus@yandex.ru>
2025-09-01 21:22:34 +0300

    feat(Views): Add Game page

---
+++ Controllers/GameController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Potratim.Data;
+using Potratim.Models;
+
+namespace Potratim.Controllers
+{
+    [Route("[controller]")]
+    public class GameController : Controller
+    {
+        private readonly ILogger<GameController> _logger;
+        private readonly PotratimDbContext _context;
+
+        private Game _game;
+
+        public GameController(ILogger<GameController> logger, PotratimDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            _game = _context.Games.FirstOrDefault();
+            ViewBag.Game = _game;
+
+            return View();
+        }
+    }
+}
