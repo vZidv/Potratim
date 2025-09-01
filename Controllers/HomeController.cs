d490038f2ddf2ff92f4687258ce4c10d30af2eea
vZidv <diz.shulinus@yandex.ru>
2025-09-01 13:40:45 +0300

    feat(Database): Password add to user-secrets

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Potratim.Data;
 using Potratim.Models;
 
 namespace Potratim.Controllers;
@@ -7,14 +8,32 @@ namespace Potratim.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly PotratimDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, PotratimDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
+        return View();
+    }
+
+    public async Task<IActionResult> SuperIndex()
+    {
+        try
+        {
+            Category category = new() { Id = 1, Name = "Tets Category" };
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            ViewBag.Message = "Категория успешно добавлена";
+        }
+        catch (Exception ex)
+        {
+            ViewBag.Message = $"Ошибка при работе с БД: {ex.Message}";
+        }
         return View();
     }
 
--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
 using Potratim.Data;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = configuration.GetConnectionString(nameof(PotratimDbContext));
+var dbPassword = builder.Configuration["DatabasePassword"];
+var fullConnectionString = new NpgsqlConnectionStringBuilder(connectionString)
+{
+    Password = dbPassword
+};
+
 builder.Services.AddDbContext<PotratimDbContext>(options =>
 {
-    options.UseNpgsql(configuration.GetConnectionString(nameof(PotratimDbContext)));
+    options.UseNpgsql(fullConnectionString.ToString());
 });
 
 var app = builder.Build();
