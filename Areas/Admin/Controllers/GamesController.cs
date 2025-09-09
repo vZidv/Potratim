5a84a447411396a02c74672f9f6b00d57c637231
vZidv <diz.shulinus@yandex.ru>
2025-09-09 22:07:57 +0300

    feat(Admin): Add Games page and ways to add and delete

---
+++ Areas/Admin/Controllers/GamesController.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Scaffolding;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Potratim.Data;
+using Potratim.Models;
+using Potratim.ViewModel;
+
+namespace Potratim.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GamesController : Controller
+    {
+        private readonly PotratimDbContext _context;
+        private readonly IWebHostEnvironment _environment;
+
+        public GamesController(PotratimDbContext context, IWebHostEnvironment environment)
+        {
+            _context = context;
+            _environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            var games = _context.Games.ToList();
+            return View(games);
+        }
+        /////////////////////////////////////////////////////////
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateGameViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var imageUrl = await SaveFile(model.ImageFile, model.Title);
+
+                Game game = new()
+                {
+                    Title = model.Title,
+                    Description = model.Description,
+                    ReleaseDate = model.ReleaseDate,
+                    Developer = model.Developer,
+                    Publisher = model.Publisher,
+                    Price = model.Price,
+                    ImageUrl = imageUrl
+                };
+                _context.Games.Add(game);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(Guid Id)
+        {
+            var game = await _context.Games.FindAsync(Id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            return View(game);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Game game)
+        {
+            if (!String.IsNullOrWhiteSpace(game.ImageUrl))
+            {
+                var imagePath = Path.Combine(_environment.WebRootPath, game.ImageUrl);
+                if (System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+            }
+
+            _context.Games.Remove(game);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+
+        public async Task<string?> SaveFile(IFormFile file, string fileName)
+        {
+            string fileUrl = null;
+
+            if (file != null && file.Length > 0)
+            {
+
+                string uploadDir = Path.Combine(_environment.WebRootPath, "images", "game-images");
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" + fileName + Path.GetExtension(file.FileName);
+
+                if (!Directory.Exists(uploadDir))
+                    Directory.CreateDirectory(uploadDir);
+
+                string filePath = Path.Combine(uploadDir, uniqueFileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+                fileUrl = $"/images/game-images/{uniqueFileName}";
+            }
+
+            return fileUrl;
+        }
+    }
+}
---
+++ ViewModel/CreateGameViewModel.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Potratim.ViewModel
+{
+    public class CreateGameViewModel
+    {
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "Название")]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(1000)]
+        [Display(Name = "Описание")]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [Column(TypeName = "date")]
+        [Display(Name = "Дата релиза")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "Разработчик")]
+        public string Developer { get; set; } = null!;
+
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "Издатель")]
+        public string Publisher { get; set; } = null!;
+
+        [Required]
+        [Column(TypeName = "money")]
+        [Display(Name = "Цена")]
+        public decimal Price { get; set; }
+
+        public IFormFile? ImageFile { get; set; } = null;
+    }
+}
